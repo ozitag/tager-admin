7360ec72a530b866e8552c6041ce9dbf4e9f6d53
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-05-19 20:12:12 +0300

    Add files from letique. Display sidebar menu

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 
-Vue.config.productionTip = false;
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import App from './views/App.vue';
+// import router from './router';
+// import store from './store';
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
-  router,
-  store,
+  // router,
+  // store,
   render: h => h(App)
 }).$mount('#app');

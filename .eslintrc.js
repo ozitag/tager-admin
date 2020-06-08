be1dcbb8cab1e9a0badadb21047535c8453af23f
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-06-08 13:30:27 +0300

    Fix ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // off this rule, because we have the same rule for Typescript - "@typescript-eslint/no-unused-vars"
+    'no-unused-vars': 'off'
   }
 };

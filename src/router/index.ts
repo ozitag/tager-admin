9b2b83d3de89ddaa4a0f6e43c6b66ed4fa492b5b
Vital Ozierski <ozicoder@gmail.com>
2022-08-10 11:43:01 +0300

    Update index.ts
    add public_path to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -31,6 +31,7 @@ export const HOME_ROUTE: RouteRecordRaw = {
 };
 
 export const router = createRouter({
+  history: createWebHistory(process.env.VUE_APP_PUBLIC_PATH || "/"),
   routes: [
     HOME_ROUTE,
     // PAGE_FORM_ROUTE,
